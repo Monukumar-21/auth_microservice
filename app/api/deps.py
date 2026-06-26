c414aa5bbecafc83a980d495d5d84eb9ece7b999
Monu Kumar <monukumar216290@gmail.com>
2026-06-26 15:14:02 +0530

    RBAC added

--- app/api/deps.py
+++ app/api/deps.py
@@ -30,7 +30,7 @@ class RoleChecker:
     def __init__(self,allowed_roles:list):
         self.allowed_roles=allowed_roles
         
-    def __call__(self, current_user:dict=Depends(get_current_user())):
+    async def __call__(self, current_user:dict=Depends(get_current_user)):
         if current_user["role"] not in self.allowed_roles:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You do not have enough permissions to access this resource"
             )
